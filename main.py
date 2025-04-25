8f76d9cce766f279976aa0be364c6a6e8ddfc7c5
Ntzy123 <cs16fox@vip.qq.com>
2025-04-26 02:21:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ class Douban:
     
     def __init__(self):
         self.url = 'https://movie.douban.com/top250'
-        start_num = []
+        self.start_num = []
         for i in range(0, 226, 25):
-            start_num.append(i)
+            self.start_num.append(i)
         self.header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
         
     def get_top250(self):
         for start_index in self.start_num:
             start_index = str(start_index)
             html = requests.get(self.url,params={'start': start_index},headers=self.header)
-            soup = BeautifulSoup(html,'html.parser')
-            text = soup.select('#content > div > div.article > ol > li:nth-child(1) > div > div.info > div.hd > a > span.other')
-            print(text)
+            soup = BeautifulSoup(html.text,'html.parser')
+            text = soup.select('#content > div > div.article > ol > li > div > div.info > div.hd > a > span.other')
+            for name in text:
+                print(name.get_text())
             
             
             
