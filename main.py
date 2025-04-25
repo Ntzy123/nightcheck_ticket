4705d14df4d9ee3f9f477cd6f573dab1505646dd
Ntzy123 <cs16fox@vip.qq.com>
2025-04-26 03:46:12 +0800

    Update 2025.4.26 3:46

--- main.py
+++ main.py
@@ -25,5 +25,11 @@ class Douban:
             
             
 if __name__ == "__main__":
-    db = Douban()
-    db.get_top250()
+    #db = Douban()
+    #db.get_top250()
+    URL = "https://heimdallr.onewo.com/remote-event-center-new/#/taskDispatch/taskList"
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
+    html = requests.get(URL,headers=header)
+    soup = BeautifulSoup(html.text,'html.parser')
+    print(soup.prettify())
+
